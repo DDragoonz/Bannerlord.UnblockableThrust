111e0fbeab100a5bb848c008fcf52123b808ce0f
DDragoonz <Dragoonz.D@gmail.com>
2025-02-06 20:35:02 +0700

    add loc support

--- UnblockableThrustConfig.cs
+++ UnblockableThrustConfig.cs
@@ -11,20 +11,20 @@ namespace UnblockableThrust
         public override string FolderName => "UnblockableThrust";
         public override string FormatType => "json2";
         
-        [SettingPropertyBool("Player Only (attacking)", Order = 0, RequireRestart = false, HintText = "shall crush through only applied if dealt by player (when attacking)?")]
-        [SettingPropertyGroup("Settings", GroupOrder = 1)]
+        [SettingPropertyBool("{=UnbTh_00001}Player Only (attacking)", Order = 0, RequireRestart = false, HintText = "{=UnbTh_00002}Shall crush through only applied if dealt by player (when attacking)?")]
+        [SettingPropertyGroup("{=UnbTh_00000}Settings", GroupOrder = 1)]
         public bool PlayerOnlyAsAttacker{ get; set; } = false;
         
-        [SettingPropertyBool("Player Only (defending)", Order = 0, RequireRestart = false, HintText = "shall crush through only applied to player (when defending)? require player only attacking to be false. this means ONLY PLAYER need to parry thrust attack")]
-        [SettingPropertyGroup("Settings", GroupOrder = 1)]
+        [SettingPropertyBool("{=UnbTh_00003}Player Only (defending)", Order = 0, RequireRestart = false, HintText = "{=UnbTh_00004}Shall crush through only applied to player (when defending)? require player only attacking to be false. this means ONLY PLAYER need to parry thrust attack")]
+        [SettingPropertyGroup("{=UnbTh_00000}Settings", GroupOrder = 1)]
         public bool PlayerOnlyAsDefender{ get; set; } = false;
         
-        [SettingPropertyBool("Mounted Only", Order = 0, RequireRestart = false, HintText = "Shall crush through only applied when mounted?")]
-        [SettingPropertyGroup("Settings", GroupOrder = 1)]
+        [SettingPropertyBool("{=UnbTh_00005}Mounted Only", Order = 0, RequireRestart = false, HintText = "{=UnbTh_00006}Shall crush through only applied when mounted?")]
+        [SettingPropertyGroup("{=UnbTh_00000}Settings", GroupOrder = 1)]
         public bool MountedOnly{ get; set; } = false;
         
-        [SettingPropertyFloatingInteger("Minimum Relative Speed", 0f, 100f, "0.0", Order = 1, RequireRestart = false, HintText = "Minimum relative speed for crush through to be applied")]
-        [SettingPropertyGroup("Settings", GroupOrder = 1)]
+        [SettingPropertyFloatingInteger("{=UnbTh_00007}Minimum Relative Speed", 0f, 100f, "0.0", Order = 1, RequireRestart = false, HintText = "{=UnbTh_00008}Minimum relative speed for crush through to be applied")]
+        [SettingPropertyGroup("{=UnbTh_00000}Settings", GroupOrder = 1)]
         public float MinRelativeSpeed{ get; set; } = 0;
     }
 }
